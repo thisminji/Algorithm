1e8bbce729f9fdacbff40cc5e10b057864ca2a90
minji <165538889+thisminji@users.noreply.github.com>
2026-01-13 11:26:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.73 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,12 @@
 def solution(participant, completion):
     mp = {}
-
-    # 참가자 이름 개수 세기(+1)
     for name in participant:
-        mp[name] = mp.get(name, 0) + 1
-
-    # 완주자 이름 개수 빼기(-1)
+        mp[name]=mp.get(name,0)+1
+        
     for name in completion:
-        mp[name] -= 1
-
-    # 1 남은 사람이 미완주자
+        mp[name]-=1
+        
+        # mp.items() - 딕셔너리에 들어있는 키 값 쌍을 전부 꺼내줌
     for name, cnt in mp.items():
-        if cnt == 1:
+        if cnt==1:
             return name
